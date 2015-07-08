665929d388e1597eb2560515149991974e67fc00
Rahul Jain <rjain@pivotal.io>
2015-07-08 15:43:01 -0500

    Update README and exported the software versions

--- README.md
+++ README.md
@@ -4,6 +4,11 @@ Please edit VagrantFile and set the desired IP for you VM.
 
 Execute `vagrant up` and enjoy ELK.
 
-Logstash port is 5000 and 8000
+If you're not using vagrant and want to install ELK on ubuntu 14.04 or 15.04, then clone this repo, and execute `./scripts/install_config.sh [CONFIG_ROOT_DIR] [VM_IP_ADDRESS]`
 
+`CONFIG_ROOT_DIR` is path to the <git-clone-dir>/elk-vagrant-box directory
+
+`VM_IP_ADDRESS` is the ip address of your ubuntu machine.
+
+Logstash port are open on 5000 and 8000 (TCP and UDP) for type - syslog
 Kibana port is 5601
--- scripts/install_config.sh
+++ scripts/install_config.sh
@@ -3,25 +3,29 @@
 export CONFIG_ROOT_DIR=$1
 export IP=$2
 
+export ELASTIC_SEARCH_VERSION=1.6
+export LOG_SEARCH_VERSION=1.5
+export KIBANA_VERSION=4.1.1
+
 sudo apt-get update && sudo apt-get install openssh-server && sudo service ssh restart
 
 sudo ufw disable
 sudo add-apt-repository -y ppa:webupd8team/java
 
 wget -O - http://packages.elasticsearch.org/GPG-KEY-elasticsearch | sudo apt-key add -
-echo 'deb http://packages.elasticsearch.org/elasticsearch/1.6/debian stable main' | sudo tee /etc/apt/sources.list.d/elasticsearch.list
-echo 'deb http://packages.elasticsearch.org/logstash/1.5/debian stable main' | sudo tee /etc/apt/sources.list.d/logstash.list
+echo "deb http://packages.elasticsearch.org/elasticsearch/$ELASTIC_SEARCH_VERSION/debian stable main" | sudo tee /etc/apt/sources.list.d/elasticsearch.list
+echo "deb http://packages.elasticsearch.org/logstash/$LOG_SEARCH_VERSION/debian stable main" | sudo tee /etc/apt/sources.list.d/logstash.list
 
 sudo apt-get update
 echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections && echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
 sudo apt-get -y install oracle-java8-installer
 
-sudo apt-get -y install elasticsearch=1.6.0
+sudo apt-get -y install elasticsearch
 sudo sed -i.bak -e 's/#network.host: 192.168.0.1/network.host: localhost/g' /etc/elasticsearch/elasticsearch.yml #Set network.host: localhost
 sudo service elasticsearch restart
 sudo update-rc.d elasticsearch defaults enable 96 7
 
-cd ~; wget https://download.elasticsearch.org/kibana/kibana/kibana-4.1.0-linux-x64.tar.gz
+cd ~; wget https://download.elasticsearch.org/kibana/kibana/kibana-$KIBANA_VERSION-linux-x64.tar.gz
 tar xvf kibana-*.tar.gz
 sudo sed -i.bak -e "s/host: \"0.0.0.0\"/network.host: $IP/g" ~/kibana-4*/config/kibana.yml
 sudo mkdir -p /opt/kibana
